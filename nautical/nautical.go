748b738c67579dd3420e851fcd31acef48099be5
Kevin Gillieron <kevin.gillieron@epfl.ch>
2013-11-26 22:23:51 +0100

    made comments for the nautical package compliant with Go coding style.

--- nautical/nautical.go
+++ nautical/nautical.go
@@ -1,12 +1,12 @@
-// Nautical math conversions.
+// Package nautical provides conversion functions for nautical metrics.
 package nautical
 
-// Convert meters into nautical miles.
+// MetersToNauticalMiles converts meters into nautical miles.
 func MetersToNauticalMiles(meters float64) float64 {
 	return meters / 1852
 }
 
-// Convert nautical miles to meters.
+// NauticalMilesToMeters converts nautical miles to meters.
 func NauticalMilesToMeters(nm float64) float64 {
 	return nm * 1852
 }
